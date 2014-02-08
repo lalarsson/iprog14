defb6214187cb26a90fcbd6370421ad1fbe5fb50
Lars Larsson <lala@kth.se>
2014-02-08 15:50:09 +0100

    started writing some of the methods. will finihs work tomorrow.

--- src/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -1,12 +1,17 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import se.kth.csc.iprog.dinnerplanner.android.view.MenuChooser;
 import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 import se.kth.csc.iprog.dinnerplanner.model.Dish;
+import se.kth.csc.iprog.dinnerplanner.model.Ingredient;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.graphics.drawable.BitmapDrawable;
@@ -20,6 +25,7 @@ public class MainActivity extends Activity {
 		Dish starter = null;
 		Dish main = null;
 		Dish dessert = null;
+		Dish[] menu = new Dish[3];
 
 		// Creating the view class instance
 		DinnerModel model = ((DinnerPlannerApplication) this.getApplication())
@@ -81,26 +87,79 @@ public class MainActivity extends Activity {
 
 		setContentView(R.layout.activity_menu);
 		MenuChooser mainView = new MenuChooser(findViewById(R.id.menu), model);
-		int currentView = 0; // 0 for ingredients, 1 for start, 2 for main, 3
-								// for dessert
-
+		/* Testing purposes*/
+		int currentView = 0; // 0 for ingredients, 1 for start, 2 for main, 3 for dessert
+		starter = model.getDish("French toast", 1);
+		main = model.getDish("Meat balls", 2);
+		dessert = model.getDish("Ice Cream", 3);
+		
+		menu[0] = starter; 
+		menu[1] = main; 
+		menu[2] = dessert;
+		
+		/*Testing end*/
 		switch (currentView) {
-		case 0:
-			loadIngred(mainView, model, new Dish[] { starter, main, dessert });
+		case 0: 
+			loadIngred(mainView, model, menu);
 		case 1:
-			loadStarter(mainView, model, starter);
+			//loadStarter(mainView, model, starter);
 		case 2:
-			loadMain(mainView, model, main);
+			//loadMain(mainView, model, main);
 		case 3:
-			loadDessert(mainView, model, dessert);
+			//loadDessert(mainView, model, dessert);
 		default:
-			loadDefault(mainView);
+			//loadDefault(mainView);
 		}
 
 	}
 
-	void loadIngred(MenuChooser v, DinnerModel m, Dish[] d) {
-
+	void loadIngred(MenuChooser v, DinnerModel model, Dish[] menu) {
+		
+		Dish s = menu[0];
+		Dish m = menu[1];
+		Dish d = menu[2];
+		double price = 0;
+		Set<Ingredient> allIngreds = new HashSet<Ingredient>();
+		for(Ingredient ingred : s.getIngredients()){
+			price += ingred.getPrice();
+			allIngreds.add(ingred);
+		}
+		for(Ingredient ingred : m.getIngredients()){
+			price += ingred.getPrice();
+			allIngreds.add(ingred);
+		}
+		for(Ingredient ingred : d.getIngredients()){
+			price += ingred.getPrice();
+			allIngreds.add(ingred);
+		}
+		
+		TextView tv = (TextView) findViewById(R.id.total_cost);
+		tv.setText("Total Cost: " + price);
+		
+		TextView content = (TextView) findViewById(R.id.menu_textcontent);
+		TextView title = (TextView) findViewById(R.id.menu_textTitle);
+		title.setText("Ingredients");
+		StringBuilder sb = new StringBuilder();
+		for(Ingredient ingred : allIngreds){
+				sb.append(ingred.getName()+" "+ingred.getQuantity()+" " + ingred.getUnit() +"\n");
+		}
+		content.setText(sb.toString());
+		
+		ImageButton starter = ((ImageButton) findViewById(R.id.menu_starter));
+		starter.setImageResource(getResources().getIdentifier(s.getImage(), "drawable", getPackageName()));
+		TextView tvStarter = ((TextView) findViewById(R.id.menu_textstarter));
+		tvStarter.setText(s.getName());
+		
+		
+		ImageButton main = ((ImageButton) findViewById(R.id.menu_main));
+		main.setImageResource(getResources().getIdentifier(m.getImage(), "drawable", getPackageName()));
+		TextView tvMain = ((TextView) findViewById(R.id.menu_textmain));
+		tvMain.setText(m.getName());
+		
+		ImageButton dessert = ((ImageButton) findViewById(R.id.menu_dessert));
+		dessert.setImageResource(getResources().getIdentifier(d.getImage(), "drawable", getPackageName()));
+		TextView tvDessert = ((TextView) findViewById(R.id.menu_textdessert));
+		tvDessert.setText(d.getName());
 	}
 
 	void loadStarter(MenuChooser v, DinnerModel m, Dish d) {
--- src/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
+++ src/se/kth/csc/iprog/dinnerplanner/model/DinnerModel.java
@@ -89,7 +89,7 @@ public class DinnerModel implements IDinnerModel{
 		dish3.addIngredient(dish3ing10);
 		dish3.addIngredient(dish3ing11);
 		dishes.add(dish3);
-		
+		/*
 		Dish dish4 = new Dish("Baked Brie",Dish.DESSERT,"bakedbrie","Preheat an oven to -400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.");
 		Ingredient dish4ing1 = new Ingredient("extra lean yoghurt",115,"g",20);
 		dish2.addIngredient(dish4ing1);
@@ -99,6 +99,7 @@ public class DinnerModel implements IDinnerModel{
 		Ingredient dish5ing1 = new Ingredient("extra lean yoghurt",115,"g",20);
 		dish5.addIngredient(dish5ing1);
 		dishes.add(dish5);
+		*/
 		
 	}
 	
@@ -150,6 +151,17 @@ public class DinnerModel implements IDinnerModel{
 	@Override
 	public Dish getSelectedDish(int type) {
 		return (Dish) getDishesOfType(type);
+	}
+	
+	public Dish getDish(String name, int type){
+		Dish searchedDish = null;
+		for(Dish d : getDishesOfType(type)){
+			if(d.getName().equalsIgnoreCase(name)){
+				searchedDish = d;
+				break;
+			}
+		}
+		return searchedDish;
 	}
 
 	@Override
